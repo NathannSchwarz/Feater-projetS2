86004425e7e579ca4f0a8f2cf6e0005fa4f75ecc
Nathan <nathan.schwarz02@edu.univ-fcomte.fr>
2024-05-15 16:21:25 +0200

    ajout d'un svg

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -4,7 +4,7 @@ import SearchIcon from '@/components/icons/Iconsearch.vue'
 
 
 import { RouterLink } from 'vue-router'
-import { ref, computed, watch } from 'vue'
+import { ref } from 'vue'
 
 // Créer une référence réactive pour suivre l'état du menu
 const activeMenu = ref(false)
@@ -15,55 +15,60 @@ function closeMenu() {
 </script>
 
 <template>
-  <header class="px-6 py-2 bg-white fixed z-10 w-full flex items-stretch justify-between lg:py-0">
+  <header class="px-10 py-16 bg-white fixed z-10 w-full flex items-stretch justify-between lg:py-0">
     <div class="flex items-center">
-      <RouterLink to="/">
-        <LogoIcon />
-      </RouterLink>
+        <RouterLink to="/">
+            <LogoIcon class="w-36 h-36" />
+        </RouterLink>
     </div>
 
-    <div class="flex items-center gap-4 lg:flex-row-reverse">
+    <div class="flex items-center gap-6 lg:flex-row-reverse">
       <button>
         <SearchIcon />
       </button>
 
       <button
-        class="relative z-10 flex h-5 w-8 flex-col justify-between *:h-[2px] *:w-full *:bg-black *:transition-all *:duration-300 *:ease lg:hidden"
-        @click="activeMenu = !activeMenu"
-      >
-        <div :class="{ 'translate-y-[9px] rotate-45 !bg-white': activeMenu }"></div>
-        <div :class="{ '!bg-white opacity-0': activeMenu }"></div>
-        <div :class="{ '-translate-y-[9px] -rotate-45 !bg-white': activeMenu }"></div>
+        class="relative z-10 flex h-6 w-8 flex-col justify-between *:h-[4px]  *:w-full *:bg-black *:transition-all *:duration-300 *:ease lg:hidden"
+        @click="activeMenu = !activeMenu">
+        
+        <div :class="{ 'translate-y-[10px] rotate-45': activeMenu }" class="rounded "></div>
+        <div :class="{ 'opacity-0': activeMenu }" class="rounded "></div>
+        <div :class="{ '-translate-y-[10px] -rotate-45': activeMenu }" class="rounded "></div>
       </button>
 
       <nav
-        class="invisible fixed inset-0 bg-indigo text-2xl text-white opacity-0 transition-all duration-300 ease-in-out lg:visible lg:relative lg:flex lg:items-center lg:bg-transparent lg:text-sm lg:font-bold lg:uppercase lg:tracking-wide lg:text-black lg:opacity-100"
+        class="invisible fixed inset-0 bg-red-600 text-white text-3xl opacity-0 transition-all duration-300 ease-in-out lg:visible lg:relative lg:flex lg:items-center lg:bg-transparent lg:text-sm lg:font-bold lg:uppercase lg:tracking-wide lg:text-black lg:opacity-100"
         :class="{ '!visible opacity-100': activeMenu }"
         v-scroll-lock="activeMenu"
       >
         <ul class="mt-[25vh] mx-16 lg:m-0 lg:flex">
           <li class="menu-item">
             <RouterLink class="menu-link" to="/conservatoire" @click="closeMenu"
-              >Le conservatoire</RouterLink
+              >Accueil</RouterLink
+            >
+          </li>
+          <li class="menu-item">
+            <RouterLink class="menu-link" to="/conservatoire" @click="closeMenu"
+              >Activité</RouterLink
             >
           </li>
           <li class="menu-item">
             <RouterLink class="menu-link" to="/enseignements" @click="closeMenu"
-              >Enseignements</RouterLink
+              >Coaching</RouterLink
             >
           </li>
           <li class="menu-item">
-            <RouterLink class="menu-link" to="/events" @click="closeMenu">Agenda</RouterLink>
+            <RouterLink class="menu-link" to="/events" @click="closeMenu">Message</RouterLink>
           </li>
           <li class="menu-item">
-            <RouterLink class="menu-link" to="/contact" @click="closeMenu">Contact</RouterLink>
+            <RouterLink class="menu-link" to="/contact" @click="closeMenu">Carte</RouterLink>
           </li>
           <li class="menu-item">
             <RouterLink
               class="menu-link lg:bg-black lg:text-white lg:px-8"
               to="/inscription"
               @click="closeMenu"
-              >S'inscrire</RouterLink
+              >Abonnement</RouterLink
             >
           </li>
         </ul>
