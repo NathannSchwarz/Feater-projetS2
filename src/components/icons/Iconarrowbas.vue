3809ffa0e5c603cefffcfd931fda55200c426e4c
Nathan <nathan.schwarz02@edu.univ-fcomte.fr>
2024-05-15 14:33:51 +0200

    ajout des icons

---
+++ src/components/icons/Iconarrowbas.vue
@@ -0,0 +1,6 @@
+<template>
+  <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="25" cy="25" r="25" fill="black"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M35.5552 22.7795L25.333 33.0865L15.1107 22.7795L17.6663 20.2028L25.333 27.933L32.9996 20.2028L35.5552 22.7795Z" fill="white"/>
+</svg>
+</template>
---
+++ src/components/icons/Iconarrowhaut.vue
@@ -0,0 +1,6 @@
+<template>
+  <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="25" cy="25" r="25" fill="black"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M15.1113 30.5099L25.3335 20.2029L35.5558 30.5099L33.0002 33.0867L25.3335 25.3564L17.6669 33.0867L15.1113 30.5099Z" fill="white"/>
+</svg>
+</template>
