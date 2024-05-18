5fb368a4c0897bed1b013c9dd909c422e14ad106
Nathan <nathan.schwarz02@edu.univ-fcomte.fr>
2024-05-18 16:07:05 +0200

    ajout d'image

--- src/components/CardsAccueil.vue
+++ src/components/CardsAccueil.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-
+import { RouterLink } from 'vue-router'
 </script>
 
 <template>
@@ -9,7 +9,7 @@
         <h3 class="pb-2">Coaching</h3>
 
         <div class="relative ">
-            <img class="rounded-xl " src="/public/img/coach.webp" alt="coaching">
+            <img class="rounded-xl " src="/public/img/coéquipier.webp" alt="coaching">
             <div class=" absolute bottom-0 w-full text-white  bg-black bg-opacity-80 p-4 rounded-b-xl ">
                 <h4 class="text-sm font-bold pb-1">Séances coachées par des professionnels </h4>
                 <p class="text-xs font-extralight">Débutez, Apprenez, Perfectionnez avec des coach de qualités  </p>
