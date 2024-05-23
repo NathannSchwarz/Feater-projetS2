b5b71dbcfb48d7b4f074eae2364b7c77dc02e837
Nathan <nathan.schwarz02@edu.univ-fcomte.fr>
2024-05-23 10:56:51 +0200

    commit page compte

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -7,7 +7,7 @@ import flecheIcon from '@/components/icons/Iconflèchemenu.vue'
 
     <nav>
         <RouterLink to="#">
-            <div class="flex justify-between items-center border-t p-3 px-6 border-black ">
+            <div class="flex justify-between items-center border-t p-3  px-6 border-black ">
                 <h3 class=" font-bold ">Données personnelles</h3>
                 <flecheIcon class="rotate-180 "/>
             </div>
