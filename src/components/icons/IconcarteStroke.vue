90983900b2dc8345379db3211201da59f811d448
Nathan <nathan.schwarz02@edu.univ-fcomte.fr>
2024-05-29 10:48:27 +0200

    ajout de svg

---
+++ src/components/icons/IconcarteStroke.vue
@@ -0,0 +1,18 @@
+<template>
+  <svg id="Calque_2" data-name="Calque 2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 25.66">
+  <defs>
+    <style>
+      .cls-1 {
+        fill: none;
+        stroke: #000;
+        stroke-miterlimit: 10;
+        stroke-width: 2px;
+      }
+    </style>
+  </defs>
+  <g id="Calque_1-2" data-name="Calque 1">
+    <path class="cls-1" d="M21,24.34l-10-2.86V1.41l10,2.86v20.07ZM22.67,24.27V4.14l6.62-2.65c.82-.33,1.71.28,1.71,1.16v17.44c0,.51-.31.97-.79,1.16l-7.55,3.02h0ZM1.79,4.49l7.55-3.02v20.13l-6.62,2.65c-.82.33-1.71-.28-1.71-1.16V5.65c0-.51.31-.97.79-1.16Z"/>
+  </g>
+</svg>
+
+</template>
